8973c8d9ee410e5fa929caa80f82bdd886a48448
Charlie Henderson <2758344h@student.gla.ac.uk>
2024-03-01 01:42:23 +0000

    Clean up views.py and add navbar template

--- CheeseBoardSite/urls.py
+++ CheeseBoardSite/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from CheeseBoardSite import views
-from django.urls import include
 
 app_name = 'CheeseBoardSite'
 urlpatterns = [
--- CheeseBoardSite/views.py
+++ CheeseBoardSite/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(request):
-    # return HttpResponse("Rango says hey there partner!")
     return render(request, 'CheeseBoardSite/index.html')
