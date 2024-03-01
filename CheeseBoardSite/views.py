8536a55084942e2f338fe3c741970c9df3bed2d3
Charlie Henderson <2758344h@student.gla.ac.uk>
2024-03-01 18:04:35 +0000

    Add grid of posts to home page

--- CheeseBoardSite/views.py
+++ CheeseBoardSite/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 
 def index(request):
-    #Processing here for most popular tags
+    # Processing here for most popular tags, expected to be a list of strings 
+    # TEMP DATA
     context_dict = {'tags':[
         "Cheddar",
         "Gouda",
@@ -14,5 +15,89 @@ def index(request):
         "Havarti",
         "Gorgonzola",
         "Monterey Jack"
+    ],
+    # Processing here for most recent posts, expected to be a list of dictionaries
+    # TEMP DATA
+    'posts': [
+        {
+            "title": "Post 1",
+            "content": "This is the first post.",
+            "img": "\media\cheese.jpg"
+        },
+        {
+            "title": "Post 2",
+            "content": "This is the second post.",
+            "img": "\media\cheese.jpg"
+        },
+        {
+            "title": "Post 3",
+            "content": "This is the third post.",
+            "img": "\media\cheese.jpg"
+        },
+        {
+            "title": "Post 4",
+            "content": "This is the fourth post.",
+            "img": "\media\cheese.jpg"
+        },
+        {
+            "title": "Post 5",
+            "content": "This is the fifth post.",
+            "img": "\media\cheese.jpg"
+        },
+        {
+            "title": "Post 6",
+            "content": "This is the sixth post.",
+            "img": "\media\cheese.jpg"
+        },
+        {
+            "title": "Post 7",
+            "content": "This is the seventh post.",
+            "img": "\media\cheese.jpg"
+        },
+        {
+            "title": "Post 8",
+            "content": "This is the eighth post.",
+            "img": "\media\cheese.jpg"
+        },
+        {
+            "title": "Post 9",
+            "content": "This is the ninth post.",
+            "img": "\media\cheese.jpg"
+        },
+        {
+            "title": "Post 10",
+            "content": "This is the tenth post.",
+            "img": "\media\cheese.jpg"
+        },
+        {
+            "title": "Post 11",
+            "content": "This is the eleventh post.",
+            "img": "\media\cheese.jpg"
+        },
+        {
+            "title": "Post 12",
+            "content": "This is the twelfth post.",
+            "img": "\media\cheese.jpg"
+        },
+        {
+            "title": "Post 13",
+            "content": "This is the thirteenth post.",
+            "img": "\media\cheese.jpg"
+        },
+        {
+            "title": "Post 14",
+            "content": "This is the fourteenth post.",
+            "img": "\media\cheese.jpg"
+        },
+        {
+            "title": "Post 15",
+            "content": "This is the fifteenth post.",
+            "img": "\media\cheese.jpg"
+        },
+        {
+            "title": "Post 16",
+            "content": "This is the sixteenth post.",
+            "img": "\media\cheese.jpg"
+        },
     ]}
     return render(request, 'CheeseBoardSite/index.html', context=context_dict)
