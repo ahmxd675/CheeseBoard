6848121f6a73d0bd84cdd846df0385f47a0d7b8f
Chloe Liu <2739541l@student.gla.ac.uk>
2024-03-10 22:03:35 +0000

    made a forms.py, (User and Account)

---
+++ CheeseBoardSite/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from django.contrib.auth.models import User
+from  CheeseBoardSite.models import Account
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password',)
+
+class AccountForm(forms.ModelForm):
+    class Meta:
+        model = Account
+        fields = ('dateOfBirth','accountCreationDate','dateLastLoggedIn','profilePic',)
