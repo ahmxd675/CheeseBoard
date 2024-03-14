ccf0af2d28e4c308b956456a74858f1f9a7df9d4
“Viktoriia <victoriia.boiechko@gmail.com>
2024-03-14 22:52:57 +0000

    PostForm

--- CheeseBoardSite/forms.py
+++ CheeseBoardSite/forms.py
@@ -1,6 +1,7 @@
+from datetime import datetime
 from django import forms
 from django.contrib.auth.models import User
-from  CheeseBoardSite.models import Account
+from  CheeseBoardSite.models import Account, Post
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
@@ -18,3 +19,20 @@ class AccountForm(forms.ModelForm):
     class Meta:
         model = Account
         fields = ('dateOfBirth',)
+        
+        
+class PostForm(forms.ModelForm):
+    ID = forms.IntegerField()
+    title =forms.CharField(max_length=64, help_text="Please enter the post title.")
+    image = forms.ImageField(help_text="Please upload the image.")
+    caption = forms.CharField(max_length=265, help_text="Please enter the caption.")
+    body = forms.CharField(max_length=4096, help_text="Please enter the body of the post.")
+    likes = forms.IntegerField(widget=forms.HiddenInput, initial=0)
+    timeDate = forms.DateTimeField(widget=forms.HiddenInput, initial=datetime.now())
+    #account = 
+    #cheeses = 
+    
+    
+    class Meta:
+        model = Post
+        fields = ('ID',)
